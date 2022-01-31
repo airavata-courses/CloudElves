96f4a7875b2a636ccfa2a8758905cc1f480a0608
Ayush <ayush@Ayushs-MacBook-Air.local>
2022-01-31 09:16:23 -0500

    data ingestor microservice

--- ingestor/ingestor/ingestor/settings.py
+++ ingestor/ingestor/ingestor/settings.py
@@ -120,8 +120,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILE_DIR = ["./images/"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
--- ingestor/ingestor/ingestor/urls.py
+++ ingestor/ingestor/ingestor/urls.py
@@ -1,7 +1,7 @@
 """ingestor URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from ingestor import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('getdata/', views.ingest),
+    path('getimage/', views.image),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
