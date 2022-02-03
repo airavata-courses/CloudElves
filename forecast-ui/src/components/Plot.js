c0f73a23add7335e2cbefe864412c67673a298c2
shahnav <shahnav@iu.edu>
2022-02-02 23:00:57 -0500

    dockerized react app

--- forecast-ui/src/components/Plot.js
+++ forecast-ui/src/components/Plot.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 const Plot = ({partochild}) => {
-    // console.log(partochild);
-    return <div className="plot">{partochild}
-    <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="plotimg"></img>
+    console.log("Plotting",partochild);
+    let forecast = partochild;
+    return <div className="plot">
+    <img src={forecast.imageUrl} alt="Weather plots" style="border: 5px solid black"></img>
+    {
+        forecast.stormExist ? (<div><text>Possibility of storm</text><text>{forecast.weatherForecas["humidity"]}</text></div>)
+        :(<div><text>No possibility of a storm</text></div>)
+    }
+    
     </div>;
 };
 
